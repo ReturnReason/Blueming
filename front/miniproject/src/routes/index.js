356aa40c8807e3f183fb09bcfae723e43146f81d
ReturnReason <reasonz@daum.net>
2022-12-08 16:36:12 +0900

    feat: 회원가입 페이지 라우터 추가

--- front/miniproject/src/routes/index.js
+++ front/miniproject/src/routes/index.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import { Main } from 'pages/index';
+import SignUp from 'pages/SignUp';
 
 export default function index() {
   return (
     <Routes>
       <Route path='/' element={<Main />} />
+      <Route path='/signup' element={<SignUp />} />
     </Routes>
   );
 }
