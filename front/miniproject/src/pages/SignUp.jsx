e8f080881cccdc80df36cdc65fb7e96b1c237950
ReturnReason <reasonz@daum.net>
2022-12-09 18:00:53 +0900

    feat(SignUp): 회원가입 페이지 레이아웃 구조

--- front/miniproject/src/pages/SignUp.jsx
+++ front/miniproject/src/pages/SignUp.jsx
@@ -1,17 +1,68 @@
 import styled from 'styled-components';
-import { Form } from 'components/index';
+import { Form, Label, TextInput, Button } from 'components/index';
 
 export default function SignUp() {
   return (
     <Container>
-      <Title />
-      <Form />
+      <Title>회원가입</Title>
+      <Form>
+        <Wrapper>
+          <Label title='아이디' htmlFor='id' />
+          <div>
+            <TextInput
+              isRequired
+              name='id'
+              placeholder='아이디를 입력해주세요'
+            />
+            <Button text='중복 확인' />
+          </div>
+        </Wrapper>
+
+        <Wrapper>
+          <Label title='비밀번호' htmlFor='pw' />
+          <TextInput
+            isRequired
+            name='pw'
+            placeholder='비밀번호를 입력해주세요'
+          />
+
+          <Label title='비밀번호 확인' htmlFor='pw-check' />
+          <TextInput
+            name='pw-check'
+            placeholder='비밀번호를 다시 입력해주세요'
+            isRequired
+          />
+        </Wrapper>
+
+        <Wrapper>
+          <Label title='이메일' htmlFor='email' />
+          <div>
+            <TextInput
+              isRequired
+              name='email'
+              placeholder='이메일을 입력해주세요'
+            />
+            <Button text='이메일 인증' />
+          </div>
+        </Wrapper>
+        <Button text='회원가입'></Button>
+      </Form>
     </Container>
   );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 800px;
+  padding: 30px;
+`;
 
 const Title = styled.h2`
   font-size: 2rem;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
