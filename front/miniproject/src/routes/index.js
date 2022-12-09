05bb3eb7a9cd8fdd77393ed82c2b60490fa2393c
ReturnReason <reasonz@daum.net>
2022-12-10 04:39:43 +0900

    feat: 레이아웃 라우터 추가

--- front/miniproject/src/routes/index.js
+++ front/miniproject/src/routes/index.js
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import { Main } from 'pages/index';
 import SignUp from 'pages/SignUp';
+import Layout from 'layouts/Layout';
 
 export default function index() {
   return (
     <Routes>
-      <Route path='/' element={<Main />} />
-      <Route path='/signup' element={<SignUp />} />
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Main />} />
+        <Route path='/signup' element={<SignUp />} />
+      </Route>
     </Routes>
   );
 }
