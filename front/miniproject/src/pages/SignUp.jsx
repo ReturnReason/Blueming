c96b33edcc89fc68ed7be5a0ed020711ef26be28
ReturnReason <reasonz@daum.net>
2022-12-11 03:14:44 +0900

    refactor(SignUp): 버튼 컴포넌트 수정으로 인한 프롭스 제거

--- front/miniproject/src/pages/SignUp.jsx
+++ front/miniproject/src/pages/SignUp.jsx
@@ -9,43 +9,27 @@ export default function SignUp() {
         <Wrapper>
           <Label title='아이디' htmlFor='id' />
           <div>
-            <TextInput
-              isRequired
-              name='id'
-              placeholder='아이디를 입력해주세요'
-            />
-            <Button text='중복 확인' />
+            <TextInput isRequired name='id' placeholder='아이디를 입력해주세요' />
+            <Button>중복 확인</Button>
           </div>
         </Wrapper>
 
         <Wrapper>
           <Label title='비밀번호' htmlFor='pw' />
-          <TextInput
-            isRequired
-            name='pw'
-            placeholder='비밀번호를 입력해주세요'
-          />
+          <TextInput isRequired name='pw' placeholder='비밀번호를 입력해주세요' />
 
           <Label title='비밀번호 확인' htmlFor='pw-check' />
-          <TextInput
-            name='pw-check'
-            placeholder='비밀번호를 다시 입력해주세요'
-            isRequired
-          />
+          <TextInput name='pw-check' placeholder='비밀번호를 다시 입력해주세요' isRequired />
         </Wrapper>
 
         <Wrapper>
           <Label title='이메일' htmlFor='email' />
           <div>
-            <TextInput
-              isRequired
-              name='email'
-              placeholder='이메일을 입력해주세요'
-            />
-            <Button text='이메일 인증' />
+            <TextInput isRequired name='email' placeholder='이메일을 입력해주세요' />
+            <Button>이메일 인증</Button>
           </div>
         </Wrapper>
-        <Button text='회원가입'></Button>
+        <Button>회원가입</Button>
       </Form>
     </Container>
   );
