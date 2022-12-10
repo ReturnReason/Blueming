8880e8248135976d3ec542d4943fc42e27a4ccd1
ReturnReason <reasonz@daum.net>
2022-12-11 03:15:26 +0900

    feat(Button): 버튼 컴포넌트 type 프롭스 추가

--- front/miniproject/src/components/Button.jsx
+++ front/miniproject/src/components/Button.jsx
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 import { PRIMARY_COLOR, WHITE_COLOR } from 'components/colors/common';
 
-export default function Button({ children, bgColor, fontColor }) {
+export default function Button({ children, bgColor, fontColor, type }) {
   return (
-    <Btn bgColor={bgColor} fontColor={fontColor}>
+    <Btn type={type} bgColor={bgColor} fontColor={fontColor}>
       {children}
     </Btn>
   );
