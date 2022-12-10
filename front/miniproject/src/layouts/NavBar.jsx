b4c5f3235f983fe82349d551f691038d44d274ae
ReturnReason <reasonz@daum.net>
2022-12-11 03:11:04 +0900

    feat(NavBar): 네비게이션 바 구조 및 스타일 적용

--- front/miniproject/src/layouts/NavBar.jsx
+++ front/miniproject/src/layouts/NavBar.jsx
@@ -1,3 +1,49 @@
+import { Button } from 'components/index';
+import { PRIMARY_COLOR, WHITE_COLOR } from 'components/colors/common';
+import styled from 'styled-components';
+
 export default function NavBar() {
-  return <></>;
+  return (
+    <Nav>
+      <h1>Symbol</h1>
+      <MenuList>
+        <li>
+          <Button bgColor={WHITE_COLOR} fontColor={PRIMARY_COLOR}>
+            Sign in
+          </Button>
+        </li>
+        <li>
+          <Button bgColor={WHITE_COLOR} fontColor={PRIMARY_COLOR}>
+            Sign up
+          </Button>
+        </li>
+      </MenuList>
+    </Nav>
+  );
 }
+
+const Nav = styled.nav`
+  width: 100%;
+  height: 46px;
+  background-color: ${PRIMARY_COLOR};
+  display: flex;
+  align-items: center;
+  padding-left: 10%;
+  padding-right: 10%;
+
+  h1 {
+    color: ${WHITE_COLOR};
+  }
+`;
+
+const MenuList = styled.ul`
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+
+  li {
+    &:first-child {
+      margin-right: 15px;
+    }
+  }
+`;
