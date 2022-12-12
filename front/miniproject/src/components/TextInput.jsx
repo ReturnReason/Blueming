9fdfdbab4d4313afac5da5789d9dd4b1520ac04b
ReturnReason <reasonz@daum.net>
2022-12-12 14:53:42 +0900

    refactor(TextInput): 사용하지 않는 프롭스 삭제

--- front/miniproject/src/components/TextInput.jsx
+++ front/miniproject/src/components/TextInput.jsx
@@ -1,16 +1,11 @@
 import styled from 'styled-components';
 
 export default function TextInput({ placeholder, name, isRequired = false }) {
-  return (
-    <Input
-      required={isRequired}
-      name={name}
-      id={name}
-      placeholder={placeholder}
-    />
-  );
+  return <Input required={isRequired} name={name} id={name} placeholder={placeholder} />;
 }
 
 const Input = styled.input`
   padding: 10px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
 `;
