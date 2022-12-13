26f732bde32262de3418abfa62c7100101b92131
ReturnReason <reasonz@daum.net>
2022-12-13 17:17:44 +0900

    feat(Card): 카드 컴포넌트 추가

---
+++ front/miniproject/src/components/Card.jsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+function Card({ children, imgSrc, content }) {
+  return (
+    <CardItem>
+      <div className='cardHeader'>
+        <a></a>
+        <img src={imgSrc} alt='이미지' className='img' />
+      </div>
+      <div className='cardBody'>
+        <strong className='cardTitle'>{children}</strong>
+        <p className='cardContent'>{content}</p>
+        <span>%</span>
+        <span>원</span>
+      </div>
+    </CardItem>
+  );
+}
+
+export default Card;
+
+const CardItem = styled.section`
+  width: 240px;
+  height: 300px;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+  background: #fff;
+  margin: 0 0 24px 24px;
+
+  .img {
+    display: block;
+    background: #999;
+    width: 100%;
+    height: 200px;
+  }
+
+  .cardBody {
+    padding: 20px;
+  }
+`;
