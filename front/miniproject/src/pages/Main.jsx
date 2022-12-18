d0f7c9589e09dcd44e86befd4579dc07a3cca137
ReturnReason <reasonz@daum.net>
2022-12-18 22:24:41 +0900

    design(Main): padding 추가

--- front/miniproject/src/pages/Main.jsx
+++ front/miniproject/src/pages/Main.jsx
@@ -1,3 +1,33 @@
+import Card from 'components/Card';
+import styled from 'styled-components';
+
 export default function Main() {
-  return <>main</>;
+  return (
+    <Container>
+      <Card content={'컨텐츠'}>카드</Card>
+      <Card content={'컨텐츠'}>카드</Card>
+      <Card content={'컨텐츠'}>카드</Card>
+      <Card content={'컨텐츠'}>카드</Card>
+      <Card content={'컨텐츠'}>카드</Card>
+      <Card content={'컨텐츠'}>카드</Card>
+      <Card content={'컨텐츠'}>카드</Card>
+      <Card content={'컨텐츠'}>카드</Card>
+      <Card content={'컨텐츠'}>카드</Card>
+      <Card content={'컨텐츠'}>카드</Card>
+      <Card content={'컨텐츠'}>카드</Card>
+      <Card content={'컨텐츠'}>카드</Card>
+      <Card content={'컨텐츠'}>카드</Card>
+      <Card content={'컨텐츠'}>카드</Card>
+      <Card content={'컨텐츠'}>카드</Card>
+      <Card content={'컨텐츠'}>카드</Card>
+    </Container>
+  );
 }
+
+const Container = styled.main`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  max-width: 1000px;
+  margin: 0 auto;
+  padding-top: 100px;
+`;
