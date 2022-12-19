443539c192349d06c17b15e95fe2c9bb6e2e10a9
ReturnReason <reasonz@daum.net>
2022-12-19 18:21:43 +0900

    design(Layout): 레이아웃 Container height 추가

--- front/miniproject/src/layouts/Layout.jsx
+++ front/miniproject/src/layouts/Layout.jsx
@@ -1,13 +1,18 @@
 import NavBar from './NavBar';
 import Footer from './Footer';
 import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
 
 export default function Layout() {
   return (
-    <>
+    <Container>
       <NavBar />
       <Outlet />
       <Footer />
-    </>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  height: 100vh;
+`;
