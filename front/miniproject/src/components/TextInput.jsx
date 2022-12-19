fafaacded60098acd278704d08dcec86c6bc2ebf
ReturnReason <reasonz@daum.net>
2022-12-19 18:21:06 +0900

    feat(TextInput): width 프롭스 추가

--- front/miniproject/src/components/TextInput.jsx
+++ front/miniproject/src/components/TextInput.jsx
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
 
-export default function TextInput({ placeholder, name, isRequired = false }) {
-  return <Input required={isRequired} name={name} id={name} placeholder={placeholder} />;
+export default function TextInput({ width, placeholder, name, isRequired = false }) {
+  return (
+    <Input width={width} required={isRequired} name={name} id={name} placeholder={placeholder} />
+  );
 }
 
 const Input = styled.input`
+  width: ${({ width }) => width};
   padding: 10px;
   border-radius: 5px;
   border: 1px solid #ccc;
