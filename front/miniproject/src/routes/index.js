8e512339324431be7fa74210bfb40b3ffe51d9d0
ReturnReason <reasonz@daum.net>
2022-12-19 18:20:24 +0900

    feat: 404 페이지 라우터 추가

--- front/miniproject/src/routes/index.js
+++ front/miniproject/src/routes/index.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import { Main, SignIn, SignUp } from 'pages/index';
+import { Main, SignIn, SignUp, Error } from 'pages/index';
 import Layout from 'layouts/Layout';
 
 export default function index() {
@@ -10,6 +10,7 @@ export default function index() {
         <Route path='/signup' element={<SignUp />} />
         <Route path='/signin' element={<SignIn />} />
       </Route>
+      <Route path='*' element={<Error />} />
     </Routes>
   );
 }
