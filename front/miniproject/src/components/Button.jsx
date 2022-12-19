6173e4eb5d5ebc29254703d6ef0b952c377ae119
ReturnReason <reasonz@daum.net>
2022-12-19 18:20:43 +0900

    feat(Button): 버튼 컴포넌트 스타일 프롭스 추가

--- front/miniproject/src/components/Button.jsx
+++ front/miniproject/src/components/Button.jsx
@@ -1,19 +1,28 @@
 import styled from 'styled-components';
 import { PRIMARY_COLOR, WHITE_COLOR } from 'components/colors/common';
 
-export default function Button({ children, bgColor, fontColor, type, onClick }) {
+export default function Button({ children, bgColor, fontColor, type, onClick, width, mt }) {
   return (
-    <Btn onClick={onClick} type={type} bgColor={bgColor} fontColor={fontColor}>
+    <Btn
+      mt={mt}
+      width={width}
+      onClick={onClick}
+      type={type}
+      bgColor={bgColor}
+      fontColor={fontColor}
+    >
       {children}
     </Btn>
   );
 }
 
 const Btn = styled.button`
+  width: ${({ width }) => width};
   border-radius: 5px;
   background: ${({ bgColor }) => bgColor || PRIMARY_COLOR};
   cursor: pointer;
   color: ${({ fontColor }) => fontColor || WHITE_COLOR};
   border: none;
   padding: 5px 8px;
+  margin-top: ${({ mt }) => mt};
 `;
