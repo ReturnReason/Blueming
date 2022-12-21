cc9aa757b23eb3a5d3f89c36283f33ca4ef7622e
ReturnReason <reasonz@daum.net>
2022-12-21 18:13:01 +0900

    feat(SignIn): 아이디, 비밀번호 찾기, 회원가입 버튼 추가

--- front/miniproject/src/pages/SignIn.jsx
+++ front/miniproject/src/pages/SignIn.jsx
@@ -9,11 +9,17 @@ function SignIn() {
         <TextInput width={'100%'} name='id' />
         <Label htmlFor='pw' title='비밀번호' />
         <TextInput width={'100%'} name='pw' />
-        <div>
-          <Button mt={'20px'} width={'100%'}>
-            로그인
-          </Button>
-        </div>
+        <LoginContainer>
+          <p>
+            <a href='#'>아이디/비밀번호 찾기</a>
+          </p>
+          <p>
+            <a href='#'>회원가입</a>
+          </p>
+        </LoginContainer>
+        <Button mt={'20px'} width={'100%'}>
+          로그인
+        </Button>
       </form>
     </Container>
   );
@@ -23,6 +29,16 @@ export default SignIn;
 
 const Container = styled.div`
   width: 400px;
-  padding: 20px;
+  padding: 30px;
   margin: 0 auto;
+  border: 1px solid #eee;
+  border-radius: 5px;
+`;
+
+const LoginContainer = styled.div`
+  display: flex;
+  width: 100%;
+  gap: 10px;
+  justify-content: space-around;
+  margin-top: 15px;
 `;
