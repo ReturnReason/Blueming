8ab451b4d569b792046dabe5ab2da4ec02fb74ad
ReturnReason <reasonz@daum.net>
2022-12-26 19:00:01 +0900

    feat: signup 라우터 추가

--- back/server.js
+++ back/server.js
@@ -3,9 +3,14 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const port = process.env.PORT;
 
 const mongoose = require('mongoose');
+const { User } = require('./models/User');
+
 mongoose
   .connect(process.env.DB_URL)
   .then(() => {
@@ -19,4 +24,13 @@ app.listen(port, () => {
 
 app.get('/', (req, res) => {
   res.send('서버 연결 테스트');
+});
+
+app.post('/signup', (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true, message: '회원가입 완료!' });
+  });
 });
