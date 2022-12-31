a83768a77d24dd41692cd7d12f0aa45bda5936d0
ReturnReason <reasonz@daum.net>
2022-12-31 21:13:26 +0900

    feat: donation 라우터 추가

--- front/src/routes/index.js
+++ front/src/routes/index.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import { Main, SignIn, SignUp, Error } from 'pages/index';
+import { Main, SignIn, SignUp, Error, Donation } from 'pages/index';
 import Layout from 'layouts/Layout';
 
 export default function index() {
@@ -9,6 +9,7 @@ export default function index() {
         <Route index element={<Main />} />
         <Route path='/signup' element={<SignUp />} />
         <Route path='/signin' element={<SignIn />} />
+        <Route path='/donation' element={<Donation />} />
       </Route>
       <Route path='*' element={<Error />} />
     </Routes>
